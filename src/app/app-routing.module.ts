3e16e752bc5f4e969aed9dc105ce9b3224cbcc1b
DasariLeelaPrasanth <leelaprasanthdasari@gmail.com>
2023-01-25 21:40:24 +0530

    layout for the ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SalesComponent } from './components/sales/sales.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'sales', component: SalesComponent },
+  { path: '',   redirectTo: '/login', pathMatch: 'full' }, // redirect to `login`
+  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SalesComponent } from './components/sales/sales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InventoryComponent,
+    DashboardComponent,
+    SalesComponent
   ],
   imports: [
     BrowserModule,
